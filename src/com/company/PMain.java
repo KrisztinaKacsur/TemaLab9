df15292388ad7663a744c3468ce9943f203f39c5
KrisztinaKacsur <kacsurkrisztina@gmail.com>
2020-06-15 17:59:12 +0300

    Console line program

---
+++ src/com/company/PMain.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class PMain {
+
+    public static void main(String[] args) {
+
+        Program program = new Program();
+        program.executeEverything();
+
+    }
+}
